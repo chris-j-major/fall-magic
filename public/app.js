4fbf8607a12bbb28ef19c155d5055abc48024c17
Chris M <chris.j.major@gmail.com>
2016-05-06 15:10:58 +0100

    dragable working

--- public/app.js
+++ public/app.js
@@ -4,6 +4,8 @@
   var $main = $("#main");
   var $toolbox = $("#toolbox");
   var $compose = $("#compose");
+  var $dragOutline = $("#dragOutline");
+  var $offsetSpacer = $("#offsetSpacer");
 
   var activeComposition = new data.Composition();
   var notes = [
@@ -50,11 +52,20 @@
       var $e = $("<div>").addClass("notes");
       return $e;
     },
-    function updatePhrase( $parent , $elem , item  ){
+    function updatePhrase( $parent , $elem , item , index   ){
       $elem.text( item.title )
+      jQuery.data( $elem[0] , "index" , index );
     }
   );
   notesDOM( notes );
+  $toolbox.on("mousedown",".notes",function(event){
+  event.preventDefault();
+    // start draggin these notes
+    var index = jQuery.data( $(event.currentTarget)[0] , "index" );
+    var note = notes[index];
+    startDrag( new data.Phrase( { pitch:1 , notes:note } ) , event );
+    return false;
+  })
 
   var compositionDOM = newDOMBinding( $compose ,
     function createPhrase( $parent , item){
@@ -63,10 +74,60 @@
     },
     function updatePhrase( $parent , $elem , item , index ){
       $elem.text( item.notes.title )
-      $elem.css( {left: (index * 5)+"em" , top:(item.pitch*2)+"em" });
+      $elem.css( {left: (index * 6)+"em" , top:(item.pitch*2)+"em" });
+      jQuery.data( $elem[0] , "index" , index );
     }
   );
   compositionDOM( activeComposition.phrases );
+  $compose.on("mousedown",".phrase",function(event){
+    // start draggin these notes
+    event.preventDefault();
+    var index = jQuery.data( $(event.currentTarget)[0] , "index" );
+    var phrase = activeComposition.phrases[index];
+    activeComposition.phrases.splice( index , 1 );
+    compositionDOM( activeComposition.phrases ); // update the display...
+    startDrag( phrase ,event );
+    return false;
+  });
+
+
+  var draggingPhrase = null;
+  function startDrag( phrase ,event ){
+    draggingPhrase = phrase;
+    $dragOutline.css({ left:event.pageX, top:event.pageY });
+    $(window).on("mousemove",dragMouseOver);
+    $(window).on("mouseup",dragMouseUp);
+    $dragOutline.removeClass("hidden");
+  }
+
+  function dragMouseOver(event){
+    event.preventDefault();
+    $dragOutline.css({ left:event.pageX, top:event.pageY });
+  }
+  function dragMouseUp(event){
+    event.preventDefault();
+    $(window).off("mousemove",dragMouseOver);
+    $(window).off("mouseup",dragMouseUp);
+    $dragOutline.addClass("hidden");
+
+    /* work out where on the main canvas */
+    var x = event.pageX;
+    var y = event.pageY;
+    var target = $compose.offset();
+    if ( x > target.left && y > target.top ){
+      // in the right box - hooray!
+      var index = Math.round((x - target.left) / $offsetSpacer.width());
+      var pitch = Math.round((y - target.top) / $offsetSpacer.height());
+      draggingPhrase.pitch = pitch;
+      if ( index > activeComposition.phrases.length ){
+        activeComposition.phrases.push(draggingPhrase);
+      }else{
+        activeComposition.phrases.splice(index,0,draggingPhrase);
+      }
+      compositionDOM( activeComposition.phrases ); // update the display
+    }
+    draggingPhrase = null;
+  }
 
   $(document).ready(resize);
   $(window).resize(resize);
