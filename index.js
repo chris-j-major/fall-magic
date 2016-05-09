7f1d668c8211b546c9f6763d17ba1b05ffe4ebf7
Chris M <chris.j.major@gmail.com>
2016-05-09 17:26:05 +0100

    Better cache control

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var morgan = require('morgan');
 var app = express();
 
+const ONE_DAY = 86400000;
+
+
 function nocache(req, res, next) {
   res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
   res.header('Expires', '-1');
@@ -11,19 +14,20 @@ function nocache(req, res, next) {
 
 if (app.get('env') == 'production') {
   app.use(morgan('common', { skip: function(req, res) { return res.statusCode < 400 } }));
+  staticMaxAge = ONE_DAY;
 }else{
   app.use( nocache );
   app.use( morgan('dev') );
+  staticMaxAge = 0;
 }
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
-app.use("/public",express.static('public'));
-app.use("/common",express.static('common'));
+app.use("/public",express.static('public',{ maxAge: staticMaxAge }));
+app.use("/common",express.static('common',{ maxAge: staticMaxAge }));
 
-app.use("/dynamic", require("./private") );
-
+app.use("/dynamic", nocache , require("./private") );
 
 app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'), function () {
